69fecadceef449593ede7526bce4957d27bd5de4
pixelpoesie <github@pixelpoesie.com>
2014-05-14 17:34:23 +0200

    Added Activity for New Message Added Class Message Added Functionality

--- app/src/main/java/com/pixelpoesie/left_right/app/MainActivity.java
+++ app/src/main/java/com/pixelpoesie/left_right/app/MainActivity.java
@@ -1,5 +1,6 @@
 package com.pixelpoesie.left_right.app;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.util.TypedValue;
@@ -10,6 +11,7 @@ import android.widget.CompoundButton;
 import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.SeekBar;
+
 
 public class MainActivity extends ActionBarActivity {
 
@@ -63,24 +65,28 @@ public class MainActivity extends ActionBarActivity {
         });
     }
 
-
-
-
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main, menu);
+
+        return super.onCreateOptionsMenu(menu);
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        return id == R.id.action_settings || super.onOptionsItemSelected(item);
+        // Handle presses on the action bar items
+        switch (item.getItemId()) {
+            case R.id.action_new_message:
+                Intent newMessageIntent = new Intent(this, NewMessageActivity.class);
+
+                newMessageIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(newMessageIntent);
+
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 
     public void setTextLeft(View v){
---
+++ app/src/main/java/com/pixelpoesie/left_right/app/NewMessageActivity.java
@@ -0,0 +1,75 @@
+package com.pixelpoesie.left_right.app;
+
+import android.app.Activity;
+import android.app.ListActivity;
+import android.os.Bundle;
+import android.text.format.Time;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Adapter;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+
+public class NewMessageActivity extends Activity {
+
+    ArrayList<String> list = new ArrayList<String>();
+    ArrayAdapter<String> adapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_message);
+        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, list);
+        ListView lv = (ListView) findViewById(R.id.messageList);
+        lv.setAdapter(adapter);
+
+
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.new_message, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+
+    public void sendMyMessage(View v){
+
+        TextView tv = (TextView)findViewById(R.id.edit_message);
+
+        Time now = new Time();
+
+
+        Message myMessage = new Message(tv.getText().toString(), now,"Christoph", "Christoph");
+
+
+
+        list.add(myMessage.toString());
+
+        tv.setText("");
+
+        adapter.notifyDataSetChanged();
+
+        Toast.makeText(this, "Test", Toast.LENGTH_SHORT).show();
+    }
+}
